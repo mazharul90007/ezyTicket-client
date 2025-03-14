d85c71e9dd8954edf9cfc6c3bdffceb12b1cdee0
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-15 00:02:39 +0600

    added some section on entertaiment pages need make it more beautiful and meanint full

--- src/Pages/Entertainment/Entertainment.jsx
+++ src/Pages/Entertainment/Entertainment.jsx
@@ -1,11 +1,17 @@
-
+import EventCategoriesSection from "./EventCategoriesSection";
+import FeaturedEventsSection from "./FeaturedEventsSection";
+import HeroSection from "./HeroSection";
+import UpcomingEventsSection from "./UpcomingEventsSection";
 
 const Entertainment = () => {
-    return (
-        <div>
-            <h2>This is Entertainment Page</h2>
-        </div>
-    );
+  return (
+    <div>
+      <HeroSection></HeroSection>
+      <FeaturedEventsSection></FeaturedEventsSection>
+      <EventCategoriesSection></EventCategoriesSection>
+      <UpcomingEventsSection></UpcomingEventsSection>
+    </div>
+  );
 };
 
 export default Entertainment;
