1ce9da8ec19c9ee72dea66850105a5059e235652
mazharul90007 <mazharul90007@gmail.com>
2025-03-14 12:47:59 +0600

    custom color added in tailwind css and applied in homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <div>
-      <h2 className="text-3xl font-semibold">This is Home Page</h2>
-      <button className="btn btn-primary">Click Me</button>
+      <h2 className="text-3xl font-semibold text-main">This is Home Page</h2>
+      <button className="btn btn-primary bg-supporting">Click Me</button>
     </div>
   );
 };
