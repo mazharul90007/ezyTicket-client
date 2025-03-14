50d5703126fdf3085be71e7f7c36014473be5db4
mazharul90007 <mazharul90007@gmail.com>
2025-03-14 19:54:39 +0600

    event banner updated

--- src/Pages/Events/EventsComponents/EventBanner.jsx
+++ src/Pages/Events/EventsComponents/EventBanner.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { FaCalendarAlt, FaTicketAlt } from 'react-icons/fa';
+import { IoTicketSharp } from "react-icons/io5";
+
 
 const EventBanner = () => {
     return (
-        <div className="bg-gradient-to-r from-green-200 to-green-600 py-16 px-8 text-white text-center relative overflow-hidden">
+        <div className="bg-gradient-to-r from-green-300 to-green-500  py-16 px-8 text-white text-center relative overflow-hidden h-[1200px] md:h-[800px] lg:h-[680px] xl:h-[600px] flex justify-center items-center">
             {/* Background Overlay */}
             <div className="absolute inset-0 bg-black opacity-30"></div>
  
             {/* Content */}
-            <div className="relative z-10">
+            <div className="relative z-10 ">
                 <h1 className="text-4xl md:text-5xl font-bold mb-4">
                     Book Your Next Event with Ease!
                 </h1>
                 <p className="text-lg md:text-xl mb-8">
                     Find, book, and manage events effortlessly.
                 </p>
-                <button className="btn btn-primary btn-lg">
+                <button className="py-2 px-4 bg-supporting flex items-center rounded">
                     <FaTicketAlt className="mr-2" /> Get Started
                 </button>
             </div>
 
             {/* Icons for Decoration */}
-            <FaCalendarAlt className="absolute top-4 left-4 text-white opacity-20 w-16 h-16" />
-            <FaCalendarAlt className="absolute bottom-4 right-4 text-white opacity-20 w-16 h-16" />
+            <IoTicketSharp className="absolute top-10 left-10 text-white opacity-20 w-32 h-32" />
+            <IoTicketSharp className="absolute bottom-10 right-10 text-white opacity-20 w-32 h-32" />
         </div>
     );
 };
