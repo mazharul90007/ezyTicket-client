0fa475154b573e38ecde55efdbd13683745b986b
MahdiHassanGo <contact.mahdiasif@gmail.com>
2025-03-14 15:58:40 +0600

    added event cards

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,10 +1,13 @@
 import EventBanner from "./EventsComponents/EventBanner";
+import EventCards from "./EventsComponents/EventCards";
 
 const Events = () => {
     return (
         <div>
             <EventBanner></EventBanner>
             <h2>This is Event Page</h2>
+
+            <EventCards></EventCards>
         </div>
     );
 };
