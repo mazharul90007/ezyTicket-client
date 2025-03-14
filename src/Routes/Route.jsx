bd937ef529f4397c6d71f44ba444bf0bff7f9644
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-14 16:22:06 +0600

    added login route

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,7 +1,4 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
@@ -11,33 +8,41 @@ import Entertainment from "../Pages/Entertainment/Entertainment";
 import Error from "../Pages/Error/Error";
 
 const Route = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout></MainLayout>,
-        errorElement: <Error></Error>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/about',
-                element: <About/>
-            },
-            {
-                path: '/travel',
-                element: <Travel/>
-            },
-            {
-                path: '/events',
-                element: <Events></Events>
-            },
-            {
-                path: '/entertainment',
-                element: <Entertainment></Entertainment>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <About />,
+      },
+      {
+        path: "/register",
+        element: <About />,
+      },
+      {
+        path: "/travel",
+        element: <Travel />,
+      },
+      {
+        path: "/events",
+        element: <Events></Events>,
+      },
+      {
+        path: "/entertainment",
+        element: <Entertainment></Entertainment>,
+      },
+    ],
+  },
+]);
 
 export default Route;
