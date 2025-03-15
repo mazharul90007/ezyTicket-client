11b7c2992c1462e4c3d204e8d215f17a50b2c55c
mazharul90007 <mazharul90007@gmail.com>
2025-03-15 19:40:49 +0600

    Pricing Page Banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>
+    <div className="pt-20">
       <h2 className="text-3xl font-semibold text-main">This is Home Page</h2>
       <button className="btn btn-primary bg-supporting">Click Me</button>
     </div>
