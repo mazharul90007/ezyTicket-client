7e183b692b6e537212c2b875f956c1d4c6141490
mazharul90007 <mazharul90007@gmail.com>
2025-03-16 21:28:41 +0600

    React fast marquee installed and marquee part added in Pricing section

--- src/Pages/Pricing/PricingComponents/PricingBanner.jsx
+++ src/Pages/Pricing/PricingComponents/PricingBanner.jsx
@@ -3,8 +3,8 @@ import ticket from "../../../assets/Pricing_image/ticket.png";
 
 const PricingBanner = () => {
     return (
-        <div className=" bg-gradient-to-br from-green-300 via-green-50 to-green-200 ">
-            <div className=" grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto gap-10 py-16 items-center">
+        <div className="min-h-[500px] md:min-h-[600px] lg:min-h-[680px] xl:min-h-[600px] bg-gradient-to-br from-green-300 via-green-50 to-green-200 pt-16">
+            <div className=" grid grid-cols-1 md:grid-cols-2 w-11/12 mx-auto gap-10 py-16 md:py-20 items-center">
                 {/* Text Content */}
                 <div className="space-y-6 md:space-y-8 text-center md:text-left">
                     <h2 className="text-4xl md:text-5xl font-bold text-gray-800">
@@ -19,15 +19,13 @@ const PricingBanner = () => {
                     </button>
                 </div>
 
-                <div className="">
-                    {/* Image */}
-                    <div className="md:w-10/12 mx-auto flex items-center justify-center bg-green-200 py-8 rounded-2xl overflow-hidden shadow">
-                        <img
-                            src={ticket}
-                            alt="ticket"
-                            className="w-64 md:w-72 h-auto transform hover:scale-105 transition-transform"
-                        />
-                    </div>
+                {/* Image */}
+                <div className="flex items-center justify-center bg-green-200 py-8 rounded-2xl overflow-hidden shadow">
+                    <img
+                        src={ticket}
+                        alt="ticket"
+                        className="w-72 md:w-80 h-auto transform hover:scale-105 transition-transform"
+                    />
                 </div>
             </div>
         </div>
