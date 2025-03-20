f9befa37763fa9fa40f424e1f8f24f879c75d2f5
Sushanta65 <sushanta9119@gmail.com>
2025-03-20 23:36:00 +0600

    conflict resolved

--- src/Pages/Events/EventDetails/EventDetails.jsx
+++ src/Pages/Events/EventDetails/EventDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Loading from "../../../Shared/Loading/Loading";
 import Swal from "sweetalert2";
 
@@ -8,9 +8,16 @@ const EventDetails = () => {
   const [eventData, setEventData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
-    const fetchClassDetails = async () => {
+    if (!eventId) {
+      console.error("❌ eventId is missing from useParams");
+      return;
+    }
+
+    const fetchEventDetails = async () => {
       try {
+        console.log(`🔍 Fetching event details for ID: ${eventId}`);
         const response = await fetch(`http://localhost:3000/events/${eventId}`);
 
         if (!response.ok) {
@@ -18,9 +25,10 @@ const EventDetails = () => {
         }
 
         const data = await response.json();
+        console.log("✅ Event data fetched:", data);
         setEventData(data);
       } catch (error) {
-        console.error("Error fetching event details:", error);
+        console.error("❌ Error fetching event details:", error);
         setError("Failed to fetch event details. Please try again later.");
         Swal.fire("Error", "Failed to fetch event details.", "error");
       } finally {
@@ -28,48 +36,55 @@ const EventDetails = () => {
       }
     };
 
-    fetchClassDetails();
+    fetchEventDetails();
   }, [eventId]);
-  if (loading)
-    return (
-      <p>
-        <Loading></Loading>
-      </p>
-    );
+
+  if (loading) return <Loading />;
   if (error) return <p className="text-red-500">{error}</p>;
+
   return (
-    <div className="mt-40 w-11/12 mx-auto mb-10">
-      <h1 className="text-3xl font-bold text-center mb-6 dark:text-white">
-        Event Details
-      </h1>
-      <div className="card bg-white shadow-md rounded-lg p-6 dark:text-black flex md:flex-row">
-        <figure>
-          <img
-            src={eventData.photo}
-            alt={eventData.title}
-            className="w-fit h-66 px-5 object-cover rounded-lg"
-          />
-        </figure>
-        <div className="card-body p-4">
-          <h2 className="card-title text-lg">{eventData.title}</h2>
-          <p className="text-sm">
-            <strong>Name:</strong> {eventData.name}
-          </p>
-          <p className="text-sm">
-            <strong>Price:</strong> ${eventData.price}
-          </p>
-          <p className="text-sm">
-            <strong>Description:</strong> {eventData.description}
-          </p>
-          <p className="text-sm">
-            <strong>Total Enrolment:</strong> {eventData.enrollmentCount || 0}
-          </p>
-          <div className="card-actions justify-end mt-2">
-            <button className="btn btn-lg bg-blue-500 text-white">
-              Buy ticket
-            </button>
-          </div>
+    <div className="container  mx-auto w-11/12 p-6 grid grid-cols-1 lg:grid-cols-3 gap-6 mt-20 bg-gray-100 px-10">
+      {/* Left Section */}
+      <div className="lg:col-span-2">
+        <img
+          src={eventData?.photo}
+          alt={eventData?.name}
+          className="w-full h-80 object-cover rounded-lg shadow-md"
+        />
+        <div className="mt-4 bg-gradient-to-br from-black via-blue-900 to-purple-900 p-20 rounded-lg shadow">
+          <h2 className="text-2xl font-bold text-white">{eventData?.name}</h2>
+          <p className="text-white mt-2">{eventData?.description}</p>
         </div>
+      </div>
+
+      {/* Right Sidebar */}
+      <div className="bg-gradient-to-br from-black via-blue-900 to-purple-900 p-20 shadow-lg rounded-lg h-fit">
+        <h3 className="text-2xl text-white font-semibold mb-4">
+          Event Information
+        </h3>
+        <p className="text-white mt-10 text-xl">
+          <strong>Name:</strong> {eventData?.title}
+        </p>
+        <p className="text-white mt-10 text-xl">
+          <strong>Price:</strong> ${eventData?.price}
+        </p>
+        <p className="text-white mt-10 text-xl">
+          <strong>Location:</strong> {eventData?.location || "N/A"}
+        </p>
+        <p className="text-white">
+          <div className="flex justify-between mt-20">
+            <button className="py-2 md:py-3 px-4 md:px-6 bg-supporting flex items-center justify-center md:justify-start rounded-lg shadow-md hover:scale-95 transform transition-transform cursor-pointer text-white font-semibold mx-auto md:mx-0 hover:bg-main">
+              Buy Tickets
+            </button>
+
+            <Link
+              to="/events"
+              className="py-2 md:py-3 px-4 md:px-6 bg-supporting flex items-center justify-center md:justify-start rounded-lg shadow-md hover:scale-95 transform transition-transform cursor-pointer text-white font-semibold mx-auto md:mx-0 hover:bg-main"
+            >
+              Back
+            </Link>
+          </div>
+        </p>
       </div>
     </div>
   );
--- src/Pages/Pricing/PricingComponents/PricingCalculator.jsx
+++ src/Pages/Pricing/PricingComponents/PricingCalculator.jsx
@@ -1,13 +1,113 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const PricingCalculator = () => {
+    const [ticketPrice, setTicketPrice] = useState("");
+    const [numberOfTickets, setNumberOfTickets] = useState("");
+    const [totalAmount, setTotalAmount] = useState(null);
+    const [fee, setFee] = useState(null);
+    const [receiveAmount, setReceiveAmount] = useState(null);
+
+    const handleCalculate = () => {
+        const price = parseFloat(ticketPrice);
+        const tickets = parseInt(numberOfTickets);
+
+        if (isNaN(price)) {
+            toast.error("Please enter a valid ticket price.");
+            return;
+        }
+
+        if (isNaN(tickets)) {
+            toast.error("Please enter a valid number of tickets.");
+            return;
+        }
+
+        const total = price * tickets;
+        setTotalAmount(total)
+        const fee = total * 0.05; // 5% fee
+        setFee(fee);
+        const finalAmount = total - fee;
+
+        setReceiveAmount(finalAmount.toFixed(2)); // Round to 2 decimal places
+    };
+
     return (
-        <div className="mb-16">
-            <div className="md:w-9/12 mx-auto">
-                <h2 className="text-4xl font-bold text-center mb-6 text-main">Calculate your EzyTicket fees</h2>
-                <p className="text-xl text-gray-600 text-center">Plan and host in-person or virtual events effortlessly. With our powerful platform, you can create unforgettable experiences for your attendees while keeping everything simple and streamlined.</p>
-            </div>
-            <div>
-                
+        <div className="bg-background">
+            <div className="py-8 w-11/12 mx-auto">
+                <div className="md:w-9/12 mx-auto">
+                    <h2 className="text-3xl md:text-4xl font-bold text-center mb-6 text-main">
+                        Calculate your EzyTicket fees
+                    </h2>
+                    <p className="text-xl text-gray-500 text-center">
+                        Plan and host in-person or virtual events effortlessly. With our powerful platform, you can create unforgettable experiences for your attendees while keeping everything simple and streamlined.
+                    </p>
+                </div>
+
+                {/* Price Calculation */}
+                <div className="mt-10 p-6 rounded-lg">
+                    <div className=" grid grid-cols-1 md:grid-cols-2 gap-8">
+                        {/* Ticket Price Input */}
+                        <div className="md:col-span-1 bg-white p-4 md:p-8 rounded-lg  space-y-4">
+                            <div className="flex items-center gap-4">
+                                <div>
+                                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                                        Ticket Price (BDT)
+                                    </label>
+                                    <input
+                                        type="number"
+                                        value={ticketPrice}
+                                        onChange={(e) => setTicketPrice(e.target.value)}
+                                        className="w-full md:w-fit p-2 md:p-4 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-supporting"
+                                        placeholder="Enter ticket price"
+                                    />
+                                </div>
+
+                                {/* Number of Tickets Input */}
+                                <div>
+                                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                                        Number of Tickets
+                                    </label>
+                                    <input
+                                        type="number"
+                                        value={numberOfTickets}
+                                        onChange={(e) => setNumberOfTickets(e.target.value)}
+                                        className="w-full md:w-fit p-2 md:p-4 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-supporting"
+                                        placeholder="Enter number of tickets"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Calculate Button */}
+                            <button
+                                onClick={handleCalculate}
+                                className="py-2 md:py-3 px-4 md:px-6 bg-supporting flex items-center justify-center md:justify-start rounded-lg shadow-md hover:scale-95 transform transition-transform cursor-pointer text-white font-semibold mx-auto md:mx-0"
+                            >
+                                Calculate
+                            </button>
+                        </div>
+
+                        <div className="md:col-span-1 bg-white p-4 md:p-8 rounded-lg">
+                            {/* Display Result */}
+                                <div className="grid grid-cols-2 gap-4">
+                                    <div className="border border-gray-300 rounded shadow p-4 md:p-8 text-center space-y-2">
+                                        <p>Total Amount</p>
+                                        <h2 className="text-4xl font-bold">৳{totalAmount || "0"}</h2>
+                                    </div>
+
+                                    <div className="border border-gray-300 rounded shadow p-4 md:p-8 text-center space-y-2">
+                                        <p>Our Charge (5%)</p>
+                                        <h2 className="text-4xl font-bold">৳{fee || "0"}</h2>
+                                    </div>
+
+                                    <div className="border border-gray-300 rounded shadow p-4 col-span-2 md:p-8 text-center space-y-2">
+                                        <p>After deducting a 5% fee, you will receive</p>
+                                        <h2 className="text-4xl font-bold text-main">৳{receiveAmount || "0"}</h2>
+                                    </div>
+                                </div>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
