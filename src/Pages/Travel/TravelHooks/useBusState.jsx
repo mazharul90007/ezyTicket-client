2f8c43624107901d2e3ec92dc500207a77594fa4
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-25 00:26:38 +0600

    fetching error with kobirul vai code

--- src/Pages/Travel/TravelHooks/useBusState.jsx
+++ src/Pages/Travel/TravelHooks/useBusState.jsx
@@ -1,33 +1,17 @@
-import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 
 const useBusState = () => {
-  const axiosSecure = useAxiosSecure();
-  const { data: busInfo = [], refetch } = useQuery({
-    queryKey: ["BusInfo"],
+  const axiosPublic = useAxiosPublic();
+  const { data: allBusData = [], refetch } = useQuery({
+    queryKey: ["allBusData"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/api/bus");
+      const res = await axiosPublic.get("/api/bus");
       return res.data;
     },
   });
 
-<<<<<<< HEAD
-    const axiosSecure = useAxiosSecure()
-    const { data: allBusData = [], refetch } = useQuery({
-        queryKey: ['allBusData'],
-        queryFn: async () => {
-            const res = await axiosSecure.get('/api/bus');
-            return res.data;
-        }
-    })
-
-    return [allBusData, refetch]
-}
-
-export default useBusState
-=======
-  return [busInfo, refetch];
+  return [allBusData, refetch];
 };
 
 export default useBusState;
->>>>>>> e1944cf (some changes)
