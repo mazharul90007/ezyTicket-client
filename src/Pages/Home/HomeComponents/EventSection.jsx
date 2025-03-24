b1365f933added514254217be557cf29e9b5c99e
mazharul90007 <mazharul90007@gmail.com>
2025-03-24 21:15:57 +0600

    event section card in homepage updated

--- src/Pages/Home/HomeComponents/EventSection.jsx
+++ src/Pages/Home/HomeComponents/EventSection.jsx
@@ -18,11 +18,10 @@ const EventSection = () => {
 
     return (
         <div className="py-16 w-11/12 mx-auto">
-            <p className="text-xl text-supporting font-semibold mb-1">Events</p>
+            <p className="text-xl text-supporting font-semibold mb-1 border-l-4 border-supporting pl-2">Events</p>
             <h3 className="text-4xl font-bold">Buy Events Ticket Easily</h3>
             <div className="my-8">
                 <div className="grid grid-cols-1 md:grid-cols-12 gap-4 h-[500px]">
-                    {/* Left Column - Single Event */}
                     {/* Left Column - Single Event */}
                     <div className="md:col-span-6 h-full">
                         {events[0] &&
@@ -35,9 +34,10 @@ const EventSection = () => {
                                             className="w-full h-full object-cover"
                                         />
                                     </figure>
-                                    <div className="absolute inset-0 bg-black/40"></div> {/* Overlay */}
+                                    <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-gray-200/20"></div>
+                                    {/* Overlay */}
                                     <div className="absolute inset-0 flex flex-col items-center justify-end text-white px-4 text-center p-2">
-                                        <h2 className="text-3xl font-bold mb-2">{events[0].title}</h2>
+                                        <h2 className="text-3xl font-bold mb-8">{events[0].title}</h2>
                                         <div className="flex w-full justify-between items-center text-xl">
                                             <div className="flex items-center gap-1">
                                                 <MdDateRange />
@@ -65,7 +65,7 @@ const EventSection = () => {
                                 <div className="relative rounded-xl overflow-hidden shadow-md hover:scale-105 transform transition-transform cursor-pointer flex flex-col h-full">
                                     {/* Image with Overlay */}
                                     <img src={event.photo} alt="events" className="w-full h-full object-cover flex-1" />
-                                    <div className="absolute inset-0 bg-black/40"></div> {/* Overlay */}
+                                    <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-gray-200/20"></div> {/* Overlay */}
 
                                     {/* Text Content */}
                                     <div className="absolute inset-0 flex flex-col items-center justify-end text-white px-4 text-center p-2">
