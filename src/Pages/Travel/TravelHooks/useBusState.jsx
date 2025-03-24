bbe6ecfc3fe203bc4bc23e8db930a47a694e2d3d
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-25 00:07:58 +0600

    some changes

--- src/Pages/Travel/TravelHooks/useBusState.jsx
+++ src/Pages/Travel/TravelHooks/useBusState.jsx
@@ -1,8 +1,17 @@
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
-import {useQuery} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 
 const useBusState = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: busInfo = [], refetch } = useQuery({
+    queryKey: ["BusInfo"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/api/bus");
+      return res.data;
+    },
+  });
 
+<<<<<<< HEAD
     const axiosSecure = useAxiosSecure()
     const { data: allBusData = [], refetch } = useQuery({
         queryKey: ['allBusData'],
@@ -16,3 +25,9 @@ const useBusState = () => {
 }
 
 export default useBusState
+=======
+  return [busInfo, refetch];
+};
+
+export default useBusState;
+>>>>>>> e1944cf (some changes)
--- src/Pages/Travel/TravelTicekBook/TravelBusTicketPage.jsx
+++ src/Pages/Travel/TravelTicekBook/TravelBusTicketPage.jsx
@@ -1,14 +1,10 @@
 import travelBannerImage from "../../../assets/Travel_image/travel-service/bg-bus.jpg"
-import useTravelContext from "../../../Hooks/TrevalHook/useTravelContext"
 import BusCard from "../TravelComponents/BusCard"
+import useBusState from "../TravelHooks/useBusState"
 import SelectPlaceTime from "./SelectPlaceTime"
 const TravelBusTicketPage = () => {
-  
-  const {allBusData} = useTravelContext()
-  const timeArray = [
-    "6:30am", "7:30am", "8:30am","9:30am", "10:30am", "11:30am", "12:30pm", "01:30pm","02:30pm","03:30pm","04:30pm","05:30pm","06:30pm","07:30pm","08:30pm","09:30pm","10:30pm","11:30pm","12:30pm",
-  ];
-
+  const [busInfo] = useBusState()
+  console.log(busInfo)
   return (
     <div className="my-20">
       <div
@@ -20,13 +16,10 @@ const TravelBusTicketPage = () => {
         </div>
       </div>
 
-      <div className="px-4 md:px-20 flex flex-col gap-10 my-14">
+      <div>
         {
-          timeArray.map((time,idx)=> allBusData.map((bus,idx)=><BusCard key={idx} bus={bus} time={time} /> ))
+          busInfo.map((bus,idx)=><BusCard key={idx} bus={bus}/> )
         }
-        {/* {
-          allBusData.map((bus,idx)=><BusCard key={idx} bus={bus}/> )
-        } */}
       </div>
 
     </div>
