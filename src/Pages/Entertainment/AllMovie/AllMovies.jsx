ee6423235cfa0d614bc420b6850a1278a34436e1
Apur0y <apuroy2785@gmail.com>
2025-03-25 01:36:33 +0600

    All movie in categories

--- src/Pages/Entertainment/AllMovie/AllMovies.jsx
+++ src/Pages/Entertainment/AllMovie/AllMovies.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaArrowAltCircleRight, FaStar } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../../Hooks/useAuth";
 import { MdLocalMovies } from "react-icons/md";
-
-
-
+import "./allmovie.css";
+import Select from "react-select";
 
 export const Movies = [
   {
@@ -42,35 +41,40 @@ export const Movies = [
   {
     id: 2,
     title: "Wicked",
-    poster: "https://ionabmargit.pages.dev/hwmkbvp-wicked-2024-poster-images-wepnwrd/wicked_2024_poster.jpg",
+    poster:
+      "https://ionabmargit.pages.dev/hwmkbvp-wicked-2024-poster-images-wepnwrd/wicked_2024_poster.jpg",
     genre: "Musical, Fantasy",
     rating: 4.8,
   },
   {
     id: 3,
     title: "Den of Thieves 2: Pantera",
-    poster: "https://www.impawards.com/2024/posters/den_of_thieves_two_pantera_ver3.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/den_of_thieves_two_pantera_ver3.jpg",
     genre: "Action, Thriller",
     rating: 4.7,
   },
   {
     id: 4,
     title: "Dune: Part Two",
-    poster: "https://jeremyctaylor.pages.dev/fscda-dune-part-two-2025-hd-tyhhj/dune_part_two_2025_poster.jpg",
+    poster:
+      "https://jeremyctaylor.pages.dev/fscda-dune-part-two-2025-hd-tyhhj/dune_part_two_2025_poster.jpg",
     genre: "Sci-Fi, Adventure",
     rating: 4.9,
   },
   {
     id: 5,
     title: "Inside Out 2",
-    poster: "https://stefaniewjemie.pages.dev/qxplrwl-inside-out-2-2024-cast-images-dmxmuua/inside_out_2_2024_poster.jpg",
+    poster:
+      "https://stefaniewjemie.pages.dev/qxplrwl-inside-out-2-2024-cast-images-dmxmuua/inside_out_2_2024_poster.jpg",
     genre: "Animation, Comedy",
     rating: 4.6,
   },
   {
     id: 6,
     title: "Deadpool & Wolverine",
-    poster: "https://www.marvel.com/articles/movies/deadpool-and-wolverine-trailer-posters/deadpool_and_wolverine_poster.jpg",
+    poster:
+      "https://www.marvel.com/articles/movies/deadpool-and-wolverine-trailer-posters/deadpool_and_wolverine_poster.jpg",
     genre: "Action, Comedy",
     rating: 4.9,
   },
@@ -84,7 +88,8 @@ export const Movies = [
   {
     id: 8,
     title: "Ghostbusters: Frozen Empire",
-    poster: "https://www.impawards.com/2024/posters/ghostbusters_frozen_empire.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/ghostbusters_frozen_empire.jpg",
     genre: "Comedy, Fantasy",
     rating: 4.5,
   },
@@ -98,14 +103,16 @@ export const Movies = [
   {
     id: 10,
     title: "Kingdom of the Planet of the Apes",
-    poster: "https://www.impawards.com/2024/posters/kingdom_of_the_planet_of_the_apes.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/kingdom_of_the_planet_of_the_apes.jpg",
     genre: "Sci-Fi, Action",
     rating: 4.7,
   },
   {
     id: 11,
     title: "Godzilla x Kong: The New Empire",
-    poster: "https://www.impawards.com/2024/posters/godzilla_x_kong_the_new_empire.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/godzilla_x_kong_the_new_empire.jpg",
     genre: "Action, Sci-Fi",
     rating: 4.8,
   },
@@ -147,7 +154,8 @@ export const Movies = [
   {
     id: 17,
     title: "Mission: Impossible - Dead Reckoning Part Two",
-    poster: "https://www.impawards.com/2024/posters/mission_impossible_dead_reckoning_part_two.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/mission_impossible_dead_reckoning_part_two.jpg",
     genre: "Action, Thriller",
     rating: 4.8,
   },
@@ -161,7 +169,8 @@ export const Movies = [
   {
     id: 19,
     title: "Aquaman and the Lost Kingdom",
-    poster: "https://www.impawards.com/2024/posters/aquaman_and_the_lost_kingdom.jpg",
+    poster:
+      "https://www.impawards.com/2024/posters/aquaman_and_the_lost_kingdom.jpg",
     genre: "Action, Fantasy",
     rating: 4.5,
   },
@@ -172,29 +181,91 @@ export const Movies = [
     genre: "Family, Fantasy",
     rating: 4.7,
   },
-  
 ];
 
-
-
-
 const AllMovies = () => {
+  const [isNowActive, setisActive] = useState(true);
+  const [isComingActive, setisComingActive] = useState(false);
+  const [isAdvancedActive, setisAdvancedActive] = useState(false);
+  const [selectedLocations, setSelectedLocations] = useState([]);
 
-  const {darkMode}=useAuth()
+  const { darkMode } = useAuth();
+
+  const options = [
+    { value: "london", label: "London" },
+    { value: "new_york", label: "New York" },
+    { value: "paris", label: "Paris" },
+    { value: "tokyo", label: "Tokyo" },
+    { value: "berlin", label: "Berlin" },
+  ];
+
+  const handleNowShowing = () => {
+    setisActive(true);
+    setisComingActive(false);
+    setisAdvancedActive(false);
+  };
+
+  const handleComing = () => {
+    setisActive(false);
+    setisComingActive(true);
+    setisAdvancedActive(false);
+  };
+
+  const handleAdvanced = () => {
+    setisActive(false);
+    setisComingActive(false);
+    setisAdvancedActive(true);
+  };
 
   return (
-    <section  className={`py-24 px-4 md:px-10 bg-gradient-to-br ${
-      darkMode
-        ? "from-black via-blue-900 to-purple-900 text-white"
-        : "from-green-200 via-green-50 to-green-200 text-purple-700"
-    } `}  >
-      
-      <h2 className="text-3xl flex items-center justify-center md:text-4xl font-bold  mb-10 ">
-      <MdLocalMovies />Movies
+    <section
+      className={`py-24 px-4 md:px-10 bg-gradient-to-br ${
+        darkMode
+          ? "from-black via-blue-900 to-purple-900 text-white"
+          : "from-green-200 via-green-50 to-green-200 text-black"
+      } `}
+    >
+      <h2 className="text-3xl underline flex items-center justify-center md:text-4xl font-bold  mb-10 ">
+        <MdLocalMovies />
+        Movies
       </h2>
- 
 
-    
+      <div className="my-5  flex flex-col md:flex-row gap-6 md:gap-32">
+        <div className="flex gap-4  md:text-3xl">
+          <button
+            className={` ${isNowActive ? "border-b-2 pb-1" : ""}`}
+            onClick={() => handleNowShowing()}
+          >
+            Now Showing
+          </button>
+          |
+          <button
+            className={` ${isComingActive ? "border-b-2" : ""}`}
+            onClick={() => handleComing()}
+          >
+            Coming Soon
+          </button>
+          |
+          <button
+            className={` ${isAdvancedActive ? "border-b-2" : ""}`}
+            onClick={() => handleAdvanced()}
+          >
+            Advanced Booking
+          </button>
+        </div>
+
+        <div className="md:w-96">
+          <Select
+            options={options}
+            isMulti
+            isSearchable
+            value={selectedLocations}
+            onChange={setSelectedLocations}
+            placeholder="Select locations..."
+            className="text-black"
+          />
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4 md:gap-10 overflow-x-auto no-scrollbar md:overflow-visible">
         {Movies.map((movie) => (
           <div
@@ -213,12 +284,11 @@ const AllMovies = () => {
                 <FaStar />
                 <span>{movie.rating}</span>
               </div>
-              <Link to ={`/entertainment/allmovies/${movie.id}`}>
-              <button className="mt-4 w-full bg-green-600 hover:bg-green-700 transition text-white py-2 cursor-pointer rounded-full font-semibold">
-                Book Now
-              </button>
+              <Link to={`/entertainment/allmovies/${movie.id}`}>
+                <button className="mt-4 w-full bg-green-600 hover:bg-green-700 transition text-white py-2 cursor-pointer rounded-full font-semibold">
+                  Book Now
+                </button>
               </Link>
-             
             </div>
           </div>
         ))}
--- src/Pages/Entertainment/MovieDetails/TicketBooking.jsx
+++ src/Pages/Entertainment/MovieDetails/TicketBooking.jsx
@@ -7,19 +7,19 @@ const TicketBooking = () => {
 
   const movie = Movies.filter((movie) => movie.id == id)[0];
 
-  console.log(movie[0]);
+//   console.log(movie[0]);
 
-  const rows = [..."ABCDEFGHIJ"]; // 10 rows
-  const leftSeats = [1, 2, 3, 4]; // Left section
-  const rightSeats = [5, 6, 7, 8]; // Right section
-  const cornerSeats = [9, 10, 11, 12]; // Right section
-  const [selectedSeats, setSelectedSeats] = useState([]);
+//   const rows = [..."ABCDEFGHIJ"]; // 10 rows
+//   const leftSeats = [1, 2, 3, 4]; // Left section
+//   const rightSeats = [5, 6, 7, 8]; // Right section
+//   const cornerSeats = [9, 10, 11, 12]; // Right section
+//   const [selectedSeats, setSelectedSeats] = useState([]);
 
-  const handleSeatSelect = (seat) => {
-    setSelectedSeats((prev) =>
-      prev.includes(seat) ? prev.filter((s) => s !== seat) : [...prev, seat]
-    );
-  };
+//   const handleSeatSelect = (seat) => {
+//     setSelectedSeats((prev) =>
+//       prev.includes(seat) ? prev.filter((s) => s !== seat) : [...prev, seat]
+//     );
+//   };
 
   const [formData, setFormData] = useState({
     name: "",
@@ -40,73 +40,9 @@ const TicketBooking = () => {
   return (
     <div>
       <div className="space-y-4">
-        {/* Screen */}
-        <div className="bg-gray-700 text-white text-center py-2 rounded-md">
-          📽️ SCREEN 📽️
-        </div>
-        <div className="flex justify-around py-5">
-          <div className="grid gap-4">
-            {rows.map((row) => (
-              <div key={row} className="flex items-center justify-center gap-4">
-                {/* Row Label */}
-                <p className="w-6 text-center font-bold">{row}</p>
-
-                {/* Left Side Seats */}
-                {leftSeats.map((num) => (
-                  <button
-                    key={`${row}${num}`}
-                    onClick={() => handleSeatSelect(`${row}${num}`)}
-                    className={`btn w-10 h-10 rounded-md ${
-                      selectedSeats.includes(`${row}${num}`)
-                        ? "bg-green-500 text-white"
-                        : "bg-gray-300 hover:bg-gray-400"
-                    }`}
-                  >
-                    {row}
-                    {num}
-                  </button>
-                ))}
-
-                {/* Aisle (Gap) */}
-                <div className="w-10"></div>
-
-                {/* Right Side Seats */}
-                {rightSeats.map((num) => (
-                  <button
-                    key={`${row}${num}`}
-                    onClick={() => handleSeatSelect(`${row}${num}`)}
-                    className={`btn w-10 h-10 rounded-md ${
-                      selectedSeats.includes(`${row}${num}`)
-                        ? "bg-green-500 text-white"
-                        : "bg-gray-300 hover:bg-gray-400"
-                    }`}
-                  >
-                    {row}
-                    {num}
-                  </button>
-                ))}
-                <div className="w-10"></div>
-                {cornerSeats.map((num) => (
-                  <button
-                    key={`${row}${num}`}
-                    onClick={() => handleSeatSelect(`${row}${num}`)}
-                    className={`btn w-10 h-10 rounded-md ${
-                      selectedSeats.includes(`${row}${num}`)
-                        ? "bg-green-500 text-white"
-                        : "bg-gray-300 hover:bg-gray-400"
-                    }`}
-                  >
-                    {row}
-                    {num}
-                  </button>
-                ))}
-              </div>
-            ))}
-          </div>
-
           {/* Booking form */}
           <section>
-            <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-2xl mx-auto mt-10">
+            <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-11/12 md:w-2xl pb-8 mx-auto mt-10">
               <h2 className="text-2xl font-bold text-center mb-4">
                 🎟 Book Your Ticket
               </h2>
@@ -192,7 +128,7 @@ const TicketBooking = () => {
           </section>
         </div>
       </div>
-    </div>
+    
   );
 };
 
