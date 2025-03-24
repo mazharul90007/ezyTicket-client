2f66b8defc4f7053d6eafd8131a0539a13e672d7
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-25 00:10:21 +0600

    firebase deploy

--- src/Pages/Travel/TravelTicekBook/TravelBusTicketPage.jsx
+++ src/Pages/Travel/TravelTicekBook/TravelBusTicketPage.jsx
@@ -1,15 +1,20 @@
-import travelBannerImage from "../../../assets/Travel_image/travel-service/bg-bus.jpg"
-import BusCard from "../TravelComponents/BusCard"
-import useBusState from "../TravelHooks/useBusState"
-import SelectPlaceTime from "./SelectPlaceTime"
+import travelBannerImage from "../../../assets/Travel_image/travel-service/bg-bus.jpg";
+import BusCard from "../TravelComponents/BusCard";
+import useBusState from "../TravelHooks/useBusState";
+import SelectPlaceTime from "./SelectPlaceTime";
 const TravelBusTicketPage = () => {
-  const [busInfo] = useBusState()
-  console.log(busInfo)
+  const [busInfo] = useBusState();
+  console.log(busInfo);
   return (
     <div className="my-20">
       <div
         className="relative hero min-h-[400px] flex items-center justify-center"
-        style={{ backgroundImage: `url(${travelBannerImage})`, backgroundSize: "cover", backgroundPosition: "center" }}>
+        style={{
+          backgroundImage: `url(${travelBannerImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="absolute inset-0 "></div>
         <div className="text-center relative z-10">
           <SelectPlaceTime />
@@ -17,13 +22,12 @@ const TravelBusTicketPage = () => {
       </div>
 
       <div>
-        {
-          busInfo.map((bus,idx)=><BusCard key={idx} bus={bus}/> )
-        }
+        {busInfo.map((bus, idx) => (
+          <BusCard key={idx} bus={bus} />
+        ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default TravelBusTicketPage
+export default TravelBusTicketPage;
