ca54cb0e4d7b49486d33b11e02bceeef53bb2eb8
Tonmoy <tonmoyhosen767@gmail.com>
2025-04-08 17:27:31 +0600

    added user data in database

--- src/API/Utils.js
+++ src/API/Utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const saveUserInformation = async (user) => {
-  axios.post(`${import.meta.env.VITE_API_URL}/users/${user?.email}`, {
+  axios.post(`http://localhost:3000/users/${user?.email}`, {
     name: user?.displayName,
     email: user?.email,
     status: "",
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 // import { useLocation } from "react-router-dom";
 import HomeBanner from "./HomeComponents/HomeBanner";
 import HomeCategory from "./HomeComponents/HomeCategory";
 import EventSection from "./HomeComponents/EventSection";
-import eidPopUp from "../../assets/Home_image/eidPopUp.gif"
+import eidPopUp from "../../assets/Home_image/eidPopUp.gif";
+import { getUserInformation } from "../../API/Utils";
 
 const Home = () => {
   // const location = useLocation();
@@ -14,7 +15,7 @@ const Home = () => {
   useEffect(() => {
     if (showModal && countdown > 0) {
       const timer = setInterval(() => {
-        setCountdown(prev => prev - 1);
+        setCountdown((prev) => prev - 1);
       }, 1000);
 
       return () => clearInterval(timer);
@@ -29,6 +30,8 @@ const Home = () => {
   };
 
   // console.log(location);
+  const result = getUserInformation();
+  console.log(result);
 
   return (
     <div className="pt-16 min-h-screen relative">
@@ -43,10 +46,18 @@ const Home = () => {
       {showModal && (
         <div className="fixed inset-0 bg-black/10 bg-opacity-70 z-50 flex items-center justify-center">
           <div className="rounded-lg mx-4">
-            <img src={eidPopUp} alt="EidPopUp" className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto max-h-[80vh] mx-auto rounded-md" />
-            <div className='flex items-center justify-between'>
+            <img
+              src={eidPopUp}
+              alt="EidPopUp"
+              className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto max-h-[80vh] mx-auto rounded-md"
+            />
+            <div className="flex items-center justify-between">
               <p className="mb-4 text-amber-600 text-lg">
-                Close in <span className="font-bold text-main text-3xl">{countdown}</span>.
+                Close in{" "}
+                <span className="font-bold text-main text-3xl">
+                  {countdown}
+                </span>
+                .
               </p>
               <button
                 onClick={handleClose}
