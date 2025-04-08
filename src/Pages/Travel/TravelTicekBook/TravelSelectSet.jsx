5c3336d5c154ebc1eed3816aafb6e2ce04717430
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-04-08 11:59:53 +0600

    create a hanlde button for send data in databse and collect essential data

--- src/Pages/Travel/TravelTicekBook/TravelSelectSet.jsx
+++ src/Pages/Travel/TravelTicekBook/TravelSelectSet.jsx
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation} from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const TravelSelectSet = () => {
-
+    const {user} = useAuth()
     const [selectedSeats, setSelectedSeats] = useState([]);
     const location = useLocation()
-    const seatPrice = 500;
-  
+    const seatPrice = location?.state?.ticketPrice;
+
     const handleSeatSelect = (seat) => {
-      setSelectedSeats((prevSeats) => {
-        if (prevSeats.includes(seat)) {
-          return prevSeats.filter((s) => s !== seat);
-        }
-        return [...prevSeats, seat];
-      });
+        setSelectedSeats((prevSeats) => {
+            if (prevSeats.includes(seat)) {
+                return prevSeats.filter((s) => s !== seat);
+            }
+            return [...prevSeats, seat];
+        });
     };
+
+    console.log(selectedSeats)
+
+
+    const handleTravelInfo = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const number = form.number.value;
+        const totalPrices = selectedSeats.length * seatPrice;
+        const passengerData = {name,email,number,selectedSeats, totalPrices, seatPrice}
+
+        Swal.fire({
+            title: "Ticket Book Successfully",
+            icon: "success",
+            draggable: true
+          });
+
+        console.log("hello", passengerData)
+    }
 
 
     return (
@@ -43,11 +66,14 @@ const TravelSelectSet = () => {
                                 {[1, 2, null, 3, 4].map((num, index) => (
                                     num ? (
                                         <button
+                                        disabled={
+                                            selectedSeats.length >= 4 && !selectedSeats.includes(`${row}${num}`)
+                                          }
                                             key={`${row}${num}`}
                                             onClick={() => handleSeatSelect(`${row}${num}`)}
                                             className={`btn rounded ${selectedSeats.includes(`${row}${num}`)
-                                                    ? "bg-main"
-                                                    : "bg-gray-200 hover:bg-main"
+                                                ? "bg-main"
+                                                : "bg-gray-200 hover:bg-main"
                                                 }`}
                                         >
                                             {row}{num}
@@ -92,36 +118,42 @@ const TravelSelectSet = () => {
                         </div>
                     </div>
                     {/* Form */}
-                    <form className="mt-6">
+                    <form onSubmit={handleTravelInfo} className="mt-6">
                         <div className="mb-4">
                             <label className="block text-sm font-medium">Passenger Name</label>
                             <input
                                 type="text"
+                                name="name"
+                                defaultValue={user?.displayName}
                                 className="w-full border border-gray-300 rounded-lg px-4 py-2 mt-1"
                                 placeholder="Enter your name"
+                                required
                             />
                         </div>
                         <div className="mb-4">
                             <label className="block text-sm font-medium">Phone Number</label>
                             <input
                                 type="tel"
+                                name="number"
                                 className="w-full border border-gray-300 rounded-lg px-4 py-2 mt-1"
                                 placeholder="Enter your phone number"
+                                required
                             />
                         </div>
                         <div className="mb-4">
                             <label className="block text-sm font-medium">Email ID</label>
                             <input
                                 type="email"
+                                name="email"
+                                defaultValue={user?.email}
                                 className="w-full border border-gray-300 rounded-lg px-4 py-2 mt-1"
                                 placeholder="Enter your email"
+                                required
                             />
                         </div>
+                        {/* Booking Button */}
+                        <input type="submit" disabled={selectedSeats.length <1} className="w-full bg-main btn  py-2 rounded-lg font-semibold" />
                     </form>
-                    {/* Booking Button */}
-                    <button className="w-full bg-main text-white py-2 rounded-lg font-semibold">
-                        Next
-                    </button>
                 </div>
             </div>
         </section>
