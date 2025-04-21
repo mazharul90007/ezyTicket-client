ecd518600e9e1d8e17e13d77c0c8755faf053cb4
Apur0y <apuroy2785@gmail.com>
2025-04-21 17:21:21 +0600

    add TMDB dynamic movies api

--- src/Provider/EntertainmentProvider.jsx
+++ src/Provider/EntertainmentProvider.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
+import axios from "axios";
 
 export const EntertainmentContext = createContext();
 
@@ -9,19 +10,37 @@ const EntertainmentProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
   const [halls, setHalls] = useState([]);
 
+  console.log(movies);
 
  const axiosSecure= useAxiosSecure();
 
-   useEffect(()=>{
+ useEffect(() => {
+  const fetchData = async () => {
+    // Get cinema halls
+    const hallsResponse = await axiosSecure.get('/cinemahalls');
+    setHalls(hallsResponse.data);
+    
+    // Get local movies
+    const localMoviesResponse = await axiosSecure.get('/allmovies');
+    const localMovies = localMoviesResponse.data;
+    
+    // Get external movies
+    const externalMoviesResponse = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=7c6a26f876561b33041c71bf76c78528");
+    const externalMovies = externalMoviesResponse.data.results;
 
-    axiosSecure.get('/cinemahalls')
-            .then(res=>
-            setHalls(res.data))
+    // const externalMoviesResponse = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=7c6a26f876561b33041c71bf76c78528");
+    // const externalMovies = externalMoviesResponse.data.results;
 
-         axiosSecure.get('/allmovies')
-         .then(res=>
-          setMovies(res.data))
-      },[])
+
+    
+    // Combine both movie sources
+    setMovies([...localMovies, ...externalMovies]);
+  };
+  
+  fetchData();
+}, [])
+
+ 
 
   const entertainmenInfo = {
     movies,
